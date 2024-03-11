819f44ac2a31cf058a1316176841fee8a87c859b
HP <panzuchao@126.com>
2024-03-11 23:58:24 +0800

    config update

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ DEVELOPMENT = "development"
 COIN_TARGET = "BTC"
 COIN_REFER = "USDT"
 
-ENV = os.getenv("ENVIRONMENT", DEVELOPMENT)
+ENV = os.getenv("ENVIRONMENT", PRODUCTION)
 DEBUG = True
 
 BINANCE = {
-  "key": "<YOUR KEY HERE>",
-  "secret": "<YOUR SECRET HERE>"
+  "key": "Gq8PfHlamdq5xK2u2SSNLTupiUC2Rxv8O2VtWTjHgFCYRtycZnNBGFZMctA9i8Y1",
+  "secret": "zouTLLMi8xdB8FMEhbuLjJPNP0SkK6EnfqnAMLGvBeSzi64MjnL8M7lggDmsWMmF"
 }
 
 TELEGRAM = {
-  "channel": "<CHANEL ID>",
-  "bot": "<BOT KEY HERE>"
+  "channel": "111",
+  "bot": "222"
 }
 
 print("ENV = ", ENV)
